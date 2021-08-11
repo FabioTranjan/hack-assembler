9f67f829719a395553b7b0c8fb7f17cec4c1c13e
Fabio <fabiotkx@gmail.com>
2021-08-10 23:38:41 -0300

    Added CompilationEngine skeleton file with described API.

---
+++ compiler/compilation_engine.rb
@@ -0,0 +1,50 @@
+class CompilationEngine
+  def initialize(input_file)
+    @input_data = File.open(input_file).read
+  end
+
+  def compile_class
+  end
+
+  def compile_class_var_dec
+  end
+
+  def compile_subroutine
+  end
+
+  def compile_parameter_list
+  end
+
+  def compile_subroutine_body
+  end
+
+  def compile_var_dec
+  end
+
+  def compile_statements
+  end
+
+  def compile_let
+  end
+
+  def compile_if
+  end
+
+  def compile_while
+  end
+
+  def compile_do
+  end
+
+  def compile_return
+  end
+
+  def compile_expression
+  end
+
+  def compile_term
+  end
+
+  def compile_expression_list
+  end
+end
