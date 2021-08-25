8e19f453423e06ce3b5541065e557dacaa8aecb2
Fabio <fabiotkx@gmail.com>
2021-08-25 10:50:46 -0300

    Implemented SymbolTable API for compiler.

---
+++ compiler/symbol_table.rb
@@ -0,0 +1,26 @@
+require './tokenizer'
+require './compilation_engine'
+
+class SymbolTable
+  def initialize
+		@symbol_table = {}
+  end
+
+	def reset
+	end
+
+	def define
+	end
+
+	def varCount
+	end
+
+	def kindOf
+	end
+
+	def typeOf
+	end
+
+	def indexOf
+	end
+end
